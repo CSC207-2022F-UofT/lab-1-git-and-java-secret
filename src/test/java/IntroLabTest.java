2ccb655692bf8163791a5186c32253fbc1da1eca
Modaser Mojadiddi <modaser.farooqi@gmail.com>
2022-09-12 12:00:38 -0400

    Added mojadid2 TestMojadid2 to IntroLab and reoevant test in IntroLabTest.

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 10)
+    public void testMojadid2(){
+        String expected = "Good Morning! My name is Modaser! " + "I like dogs more than cats! You have one class today";
+
+        String actual = IntroLab.mojadid2();
+        assertEquals(expected, actual);
+    }
 }
